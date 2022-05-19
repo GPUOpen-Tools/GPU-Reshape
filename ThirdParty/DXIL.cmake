28450c5f66cf8756822ac44376051cbb967211e2
Miguel Petersen <miguel.pemail@gmail.com>
2022-05-19 02:46:42 +0200

    [ThirdParty] Added DXIL specification puller

---
+++ ThirdParty/DXIL.cmake
@@ -0,0 +1,7 @@
+
+# DXIL Specification
+file(
+    DOWNLOAD
+    https://raw.githubusercontent.com/microsoft/DirectXShaderCompiler/v1.6.2112/docs/DXIL.rst
+    ${CMAKE_CURRENT_SOURCE_DIR}/DXIL/DXIL.rst
+)
