5e83404e92a79607b7153be41e646b1c0bd44d9e
Miguel Petersen <miguel.pemail@gmail.com>
2022-10-03 02:24:14 +0200

    [UIX] Added markdown for welcome page - Added markup FileExtension, reads files into properties - Added markdown support via Markdown.Avalonia (MIT)

---
+++ Source/UIX/Studio/MarkupExtensions/FileExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Avalonia;
+using Avalonia.Markup.Xaml;
+using Avalonia.Platform;
+
+namespace Studio.MarkupExtensions
+{
+    public class FileExtension : MarkupExtension
+    {
+        /// <summary>
+        /// Stream filename
+        /// </summary>
+        private readonly string FileName;
+
+        public FileExtension(string fileName)
+        {
+            FileName = fileName;
+        }
+        
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            // Get asset provider
+            var assets = AvaloniaLocator.Current.GetService<IAssetLoader>() ?? throw new Exception("Expected asset loader");
+
+            // Attempt to read the contents
+            using var reader = new StreamReader(assets.Open(new Uri("avares://Studio/" + FileName)));
+            return reader.ReadToEnd();
+        }
+    }
+}
---
+++ Source/UIX/Studio/Resources/Documentation/Welcome.md
@@ -0,0 +1,11 @@
+﻿# GPU Reshape
+
+It's your first day in school, and you don't know where to go.
+
+### Workspaces
+
+### Instrumentation features
+
+### Validation messages
+
+### Shader views
