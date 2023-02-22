55307c2817922b5512ed86e28d057e15c02652c4
Miguel Petersen <miguel.pemail@gmail.com>
2023-02-23 00:43:54 +0100

    [DX12] Added extension implementations for CreateCommittedResource1, CreateCommittedResource2, CreateCommittedResource3, CreatePlacedResource1, CreatePlacedResource2

--- Source/Backends/DX12/Layer/Include/Backends/DX12/Resource.h
+++ Source/Backends/DX12/Layer/Include/Backends/DX12/Resource.h
@@ -5,7 +5,10 @@
 
 /// Hooks
 HRESULT WINAPI HookID3D12DeviceCreateCommittedResource(ID3D12Device* device, const D3D12_HEAP_PROPERTIES* heap, D3D12_HEAP_FLAGS heapFlags, const D3D12_RESOURCE_DESC* desc, D3D12_RESOURCE_STATES resourceState, const D3D12_CLEAR_VALUE* clearValue, const IID& riid, void** pResource);
+HRESULT WINAPI HookID3D12DeviceCreateCommittedResource1(ID3D12Device* device, const D3D12_HEAP_PROPERTIES* pHeapProperties, D3D12_HEAP_FLAGS HeapFlags, const D3D12_RESOURCE_DESC* pDesc, D3D12_RESOURCE_STATES InitialResourceState, const D3D12_CLEAR_VALUE* pOptimizedClearValue, ID3D12ProtectedResourceSession* pProtectedSession, const IID& riidResource, void** ppvResource);
+HRESULT WINAPI HookID3D12DeviceCreateCommittedResource2(ID3D12Device* device, const D3D12_HEAP_PROPERTIES* pHeapProperties, D3D12_HEAP_FLAGS HeapFlags, const D3D12_RESOURCE_DESC1* pDesc, D3D12_RESOURCE_STATES InitialResourceState, const D3D12_CLEAR_VALUE* pOptimizedClearValue, ID3D12ProtectedResourceSession* pProtectedSession, const IID& riidResource, void** ppvResource);
 HRESULT WINAPI HookID3D12DeviceCreatePlacedResource(ID3D12Device*, ID3D12Heap*, UINT64, const D3D12_RESOURCE_DESC*, D3D12_RESOURCE_STATES, const D3D12_CLEAR_VALUE*, const IID&, void**);
+HRESULT WINAPI HookID3D12DeviceCreatePlacedResource1(ID3D12Device*, ID3D12Heap* pHeap, UINT64 HeapOffset, const D3D12_RESOURCE_DESC1* pDesc, D3D12_RESOURCE_STATES InitialState, const D3D12_CLEAR_VALUE* pOptimizedClearValue, const IID& riid, void** ppvResource);
 HRESULT WINAPI HookID3D12DeviceCreateReservedResource(ID3D12Device*, const D3D12_RESOURCE_DESC*, D3D12_RESOURCE_STATES, const D3D12_CLEAR_VALUE*, const IID&, void**);
 HRESULT WINAPI HookID3D12ResourceMap(ID3D12Resource* resource, UINT subresource, const D3D12_RANGE* readRange, void** blob);
 HRESULT WINAPI HookID3D12ResourceGetDevice(ID3D12Resource* _this, REFIID riid, void **ppDevice);
--- Source/Backends/DX12/Layer/Source/Resource.cpp
+++ Source/Backends/DX12/Layer/Source/Resource.cpp
@@ -80,6 +80,66 @@ HRESULT HookID3D12DeviceCreateCommittedResource(ID3D12Device* device, const D3D1
     return S_OK;
 }
 
+HRESULT WINAPI HookID3D12DeviceCreateCommittedResource1(ID3D12Device* device, const D3D12_HEAP_PROPERTIES* pHeapProperties, D3D12_HEAP_FLAGS HeapFlags, const D3D12_RESOURCE_DESC* pDesc, D3D12_RESOURCE_STATES InitialResourceState, const D3D12_CLEAR_VALUE* pOptimizedClearValue, ID3D12ProtectedResourceSession* pProtectedSession, const IID& riidResource, void** ppvResource) {
+    auto table = GetTable(device);
+
+    // Object
+    ID3D12Resource* resource{nullptr};
+
+    // Pass down callchain
+    HRESULT hr = table.bottom->next_CreateCommittedResource1(table.next, pHeapProperties, HeapFlags, pDesc, InitialResourceState, pOptimizedClearValue, pProtectedSession, __uuidof(ID3D12Resource), reinterpret_cast<void**>(&resource));
+    if (FAILED(hr)) {
+        return hr;
+    }
+
+    // Create state
+    resource = CreateResourceState(device, table, resource, pDesc);
+
+    // Query to external object if requested
+    if (ppvResource) {
+        hr = resource->QueryInterface(riidResource, ppvResource);
+        if (FAILED(hr)) {
+            return hr;
+        }
+    }
+
+    // Cleanup
+    resource->Release();
+
+    // OK
+    return S_OK;
+}
+
+HRESULT WINAPI HookID3D12DeviceCreateCommittedResource2(ID3D12Device* device, const D3D12_HEAP_PROPERTIES* pHeapProperties, D3D12_HEAP_FLAGS HeapFlags, const D3D12_RESOURCE_DESC1* pDesc, D3D12_RESOURCE_STATES InitialResourceState, const D3D12_CLEAR_VALUE* pOptimizedClearValue, ID3D12ProtectedResourceSession* pProtectedSession, const IID& riidResource, void** ppvResource) {
+    auto table = GetTable(device);
+
+    // Object
+    ID3D12Resource* resource{nullptr};
+
+    // Pass down callchain
+    HRESULT hr = table.bottom->next_CreateCommittedResource2(table.next, pHeapProperties, HeapFlags, pDesc, InitialResourceState, pOptimizedClearValue, pProtectedSession, __uuidof(ID3D12Resource), reinterpret_cast<void**>(&resource));
+    if (FAILED(hr)) {
+        return hr;
+    }
+
+    // Create state with lowered description
+    resource = CreateResourceState(device, table, resource, reinterpret_cast<const D3D12_RESOURCE_DESC*>(pDesc));
+
+    // Query to external object if requested
+    if (ppvResource) {
+        hr = resource->QueryInterface(riidResource, ppvResource);
+        if (FAILED(hr)) {
+            return hr;
+        }
+    }
+
+    // Cleanup
+    resource->Release();
+
+    // OK
+    return S_OK;
+}
+
 HRESULT HookID3D12DeviceCreatePlacedResource(ID3D12Device *device, ID3D12Heap * heap, UINT64 heapFlags, const D3D12_RESOURCE_DESC *desc, D3D12_RESOURCE_STATES resourceState, const D3D12_CLEAR_VALUE * clearValue, const IID& riid, void ** pResource) {
     auto table = GetTable(device);
 
@@ -98,6 +158,36 @@ HRESULT HookID3D12DeviceCreatePlacedResource(ID3D12Device *device, ID3D12Heap * 
     // Query to external object if requested
     if (pResource) {
         hr = resource->QueryInterface(riid, pResource);
+        if (FAILED(hr)) {
+            return hr;
+        }
+    }
+
+    // Cleanup
+    resource->Release();
+
+    // OK
+    return S_OK;
+}
+
+HRESULT WINAPI HookID3D12DeviceCreatePlacedResource1(ID3D12Device* device, ID3D12Heap* pHeap, UINT64 HeapOffset, const D3D12_RESOURCE_DESC1* pDesc, D3D12_RESOURCE_STATES InitialState, const D3D12_CLEAR_VALUE* pOptimizedClearValue, const IID& riid, void** ppvResource) {
+    auto table = GetTable(device);
+
+    // Object
+    ID3D12Resource* resource{nullptr};
+
+    // Pass down callchain
+    HRESULT hr = table.bottom->next_CreatePlacedResource1(table.next, pHeap, HeapOffset, pDesc, InitialState, pOptimizedClearValue, __uuidof(ID3D12Resource), reinterpret_cast<void**>(&resource));
+    if (FAILED(hr)) {
+        return hr;
+    }
+
+    // Create state with lowered description
+    resource = CreateResourceState(device, table, resource, reinterpret_cast<const D3D12_RESOURCE_DESC*>(pDesc));
+
+    // Query to external object if requested
+    if (ppvResource) {
+        hr = resource->QueryInterface(riid, ppvResource);
         if (FAILED(hr)) {
             return hr;
         }
