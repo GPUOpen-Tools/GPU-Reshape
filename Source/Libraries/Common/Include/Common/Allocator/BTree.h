e1e1272d7831c9ec2bc0f6c2197d50d046166f2b
Miguel Petersen <miguel.pemail@gmail.com>
2023-03-10 18:19:42 +0100

    [Common] Added support for BTree and UnorderedDense

---
+++ Source/Libraries/Common/Include/Common/Allocator/BTree.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+// Common
+#include "ContainerAllocator.h"
+
+// BTree
+#include <btree/fc_btree.h>
+
+/// Container vector
+template<typename K, typename V>
+using BTreeMap = frozenca::BTreeMap<K, V, 64, std::ranges::less, ContainerAllocator>;
---
+++ Source/Libraries/Common/Include/Common/Allocator/UnorderedDense.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+// Common
+#include "ContainerAllocator.h"
+
+// UnorderedDense
+#include <ankerl/unordered_dense.h>
+
+/// Container vector
+template<typename K, typename V>
+using UnorderedDense = ankerl::unordered_dense::map<K, V, ankerl::unordered_dense::hash<K>, std::equal_to<K>, ContainerAllocator<std::pair<K, V>>>;
