d438509cdce01f8015037fbf9a01df2311f4ad76
Miguel Petersen <miguel.pemail@gmail.com>
2023-06-18 19:26:31 +0200

    [Build] Updated licenses

--- Build/Utils/Copy.bat
+++ Build/Utils/Copy.bat
@@ -1,2 +1,26 @@
+rem 
+rem The MIT License (MIT)
+rem 
+rem Copyright (c) 2023 Miguel Petersen
+rem Copyright (c) 2023 Advanced Micro Devices, Inc
+rem Copyright (c) 2023 Avalanche Studios Group
+rem 
+rem Permission is hereby granted, free of charge, to any person obtaining a copy 
+rem of this software and associated documentation files (the "Software"), to deal 
+rem in the Software without restriction, including without limitation the rights 
+rem to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+rem of the Software, and to permit persons to whom the Software is furnished to do so, 
+rem subject to the following conditions:
+rem 
+rem The above copyright notice and this permission notice shall be included in all 
+rem copies or substantial portions of the Software.
+rem 
+rem THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+rem INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+rem PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+rem FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+rem ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+rem 
+
 
 xcopy "%1" "%2" /s /d /Y /i
