3420a7c446a62a3775a3a3a3ac1a45030ce82af6
Miguel Petersen <miguel.pemail@gmail.com>
2023-06-18 02:05:39 +0200

    [Build] update_license Python support

--- Build/Tools/update_license.py
+++ Build/Tools/update_license.py
@@ -1,3 +1,27 @@
+# 
+# The MIT License (MIT)
+# 
+# Copyright (c) 2023 Miguel Petersen
+# Copyright (c) 2023 Advanced Micro Devices, Inc
+# Copyright (c) 2023 Avalanche Studios Group
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# of this software and associated documentation files (the "Software"), to deal 
+# in the Software without restriction, including without limitation the rights 
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+# of the Software, and to permit persons to whom the Software is furnished to do so, 
+# subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all 
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# 
+
 import glob
 import os
 import sys
@@ -20,18 +44,19 @@ directories = [
 
 # Ignore paths
 ignore = [
-    "venv",
-    "obj",
-    "bin",
-    ".idea"
+    "/venv/",
+    "/obj/",
+    "/bin/",
+    "/.idea/"
 ]
 
 # All accepted file names
-extensions = (".h", ".cpp", ".hpp", ".inl", ".cs", ".hlsl", ".cmake")
+extensions = (".h", ".cpp", ".hpp", ".inl", ".cs", ".hlsl", ".cmake", ".py")
 
 # Alternate commenting styles
 mappings = {
-    ".cmake": "# "
+    ".cmake": "# ",
+    ".py": "# "
 }
 
 # ---------- #
@@ -89,6 +114,10 @@ for directory in directories:
         # Part of extension set?
         ext = os.path.splitext(file)[1]
         if not ext in extensions:
+            continue
+
+        # Ignored?
+        if any(ign in file.replace("\\", "/") for ign in ignore):
             continue
 
         # Determine mapping
