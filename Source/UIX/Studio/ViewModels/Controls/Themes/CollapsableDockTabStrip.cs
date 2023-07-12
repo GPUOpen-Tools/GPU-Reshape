e097d8f7c1c259430ff2ecfdd4201c7356a018c1
Miguel Petersen <miguel.pemail@gmail.com>
2023-07-13 00:23:41 +0200

    [UIX] Fixed dockable hiding, must force single selector

---
+++ Source/UIX/Studio/ViewModels/Controls/Themes/CollapsableDockTabStrip.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls;
+using Dock.Avalonia.Controls;
+
+namespace Studio.ViewModels.Controls.Themes
+{
+    public class CollapsableDockTabStrip : DockTabStrip
+    {
+        public CollapsableDockTabStrip()
+        {
+            SelectionMode = SelectionMode.Single;
+        }
+    }
+}
