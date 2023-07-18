d6365613cd4d11a0865d00cac2f5402a704f007c
Miguel Petersen <miguel.pemail@gmail.com>
2023-07-19 00:07:06 +0200

    [UIX] Tooltip / localization pass, fixed invalid entries - Added missing license
    #21

--- Source/UIX/Studio/Resources/Resources.Designer.cs
+++ Source/UIX/Studio/Resources/Resources.Designer.cs
@@ -84,6 +84,141 @@ namespace Studio.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Add a new application.
+        /// </summary>
+        public static string ApplicationList_Add_Tooltip {
+            get {
+                return ResourceManager.GetString("ApplicationList_Add_Tooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Toggle the bus mode.
+        /// </summary>
+        public static string BusMode_Toggle_Tooltip {
+            get {
+                return ResourceManager.GetString("BusMode_Toggle_Tooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Configure discoverability.
+        /// </summary>
+        public static string Discovery_DropDown_Tooltip {
+            get {
+                return ResourceManager.GetString("Discovery_DropDown_Tooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Remove this entry.
+        /// </summary>
+        public static string EditLabel_Close_Tooltip {
+            get {
+                return ResourceManager.GetString("EditLabel_Close_Tooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Display help.
+        /// </summary>
+        public static string Help_Tooltip {
+            get {
+                return ResourceManager.GetString("Help_Tooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Open a folder.
+        /// </summary>
+        public static string Launch_SelectFolder_Tooltip {
+            get {
+                return ResourceManager.GetString("Launch_SelectFolder_Tooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Clear all logging entries.
+        /// </summary>
+        public static string Log_Close_Tooltip {
+            get {
+                return ResourceManager.GetString("Log_Close_Tooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Logging filter.
+        /// </summary>
+        public static string Log_Filter_Tooltip {
+            get {
+                return ResourceManager.GetString("Log_Filter_Tooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Scroll to the bottom with new logging entries.
+        /// </summary>
+        public static string Log_ScrollToBottom_Tooltip {
+            get {
+                return ResourceManager.GetString("Log_ScrollToBottom_Tooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Clear all validation messages.
+        /// </summary>
+        public static string MessageCollection_Clear_Tooltip {
+            get {
+                return ResourceManager.GetString("MessageCollection_Clear_Tooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Filter validation messages.
+        /// </summary>
+        public static string MessageCollection_Filter_Tooltip {
+            get {
+                return ResourceManager.GetString("MessageCollection_Filter_Tooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Network data read by this session.
+        /// </summary>
+        public static string NetworkStatus_Download_Tooltip {
+            get {
+                return ResourceManager.GetString("NetworkStatus_Download_Tooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Network data written by this session.
+        /// </summary>
+        public static string NetworkStatus_Upload_Tooltip {
+            get {
+                return ResourceManager.GetString("NetworkStatus_Upload_Tooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Add a new PDB search path.
+        /// </summary>
+        public static string PDBSetting_Add_Tooltip {
+            get {
+                return ResourceManager.GetString("PDBSetting_Add_Tooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Refresh all pipeline entries.
+        /// </summary>
+        public static string PipelineTree_Refresh_Tooltip {
+            get {
+                return ResourceManager.GetString("PipelineTree_Refresh_Tooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Shader not compiled with debugging symbols, please ensure the shader is compiled with embedded debugging data..
         /// </summary>
         public static string Shader_NoDebugSymbols {
@@ -102,11 +237,29 @@ namespace Studio.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Refresh all shader entries.
+        /// </summary>
+        public static string ShaderTree_Refresh_Tooltip {
+            get {
+                return ResourceManager.GetString("ShaderTree_Refresh_Tooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Collapse all items.
         /// </summary>
         public static string Tree_Collapse_Tooltip {
             get {
                 return ResourceManager.GetString("Tree_Collapse_Tooltip", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Close all live workspaces.
+        /// </summary>
+        public static string Tree_DestroyAll_Tooltip {
+            get {
+                return ResourceManager.GetString("Tree_DestroyAll_Tooltip", resourceCulture);
             }
         }
         
