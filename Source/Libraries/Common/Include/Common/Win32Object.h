8b897da7c725c6212ae3a3ae0252e9471a305ba0
Miguel Petersen <miguel.pemail@gmail.com>
2023-08-20 15:34:17 +0200

    [Build] Submitted missing file

---
+++ Source/Libraries/Common/Include/Common/Win32Object.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+// System
+#include <handleapi.h>
+
+/// Object wrapper for releasing handles
+template<typename T>
+struct Win32Object {
+    /// Constructor
+    Win32Object(T handle) : handle(handle) {
+        
+    }
+    
+    /// Destructor
+    ~Win32Object() {
+        if (handle) {
+            CloseHandle(handle);
+        }
+    }
+
+    /// No copy/move
+    Win32Object(const Win32Object&) = delete;
+    Win32Object(Win32Object&&) = delete;
+
+    /// No copy/move assignment
+    Win32Object& operator=(const Win32Object&) = delete;
+    Win32Object& operator=(Win32Object&&) = delete;
+
+    /// Implicit object
+    operator T() const {
+        return handle;
+    }
+
+    /// Underlying object
+    T handle;
+};
+
+/// Standard objects
+using Win32Handle = Win32Object<HANDLE>;
+using Win32Module = Win32Object<HMODULE>;
