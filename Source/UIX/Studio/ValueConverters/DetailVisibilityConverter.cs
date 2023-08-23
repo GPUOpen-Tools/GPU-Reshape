df52104debd096363e2ccb0b8e24d5e9f0cb0aea
Miguel Petersen <miguel.pemail@gmail.com>
2023-08-23 16:24:47 +0200

    [UIX] Hide detail view controls when loading

---
+++ Source/UIX/Studio/ValueConverters/DetailVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using Studio.ViewModels.Workspace.Objects;
+
+namespace Studio.ValueConverters
+{
+    public class DetailVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert the value
+        /// </summary>
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            // Do not show on missing
+            if (value is MissingDetailViewModel)
+            {
+                return false;
+            }
+
+            // Is detail?
+            return value is IValidationDetailViewModel;
+        }
+
+        /// <summary>
+        /// Convert the value back
+        /// </summary>
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
