bbc8fac133f5e241ff0f9ca790c94962f0b15130
Miguel Petersen <miguel.pemail@gmail.com>
2023-11-16 22:30:18 +0100

    [BTree] Fixed git tag - Removed old typename workaround

--- ThirdParty/BTree.cmake
+++ ThirdParty/BTree.cmake
@@ -27,11 +27,11 @@
 ExternalProject_Add(
     BTree
     GIT_REPOSITORY https://github.com/frozenca/BTree
-    GIT_TAG main
+    GIT_TAG e433cbffb3a5255b33ad33d2d0458cf9a36cfbba
     SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/BTree
     USES_TERMINAL_INSTALL 0
     UPDATE_DISCONNECTED ${ThirdPartyDisconnected}
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} -DBTREE_PATH=${CMAKE_SOURCE_DIR}/ThirdParty/BTree -P "${CMAKE_SOURCE_DIR}/Build/Utils/BTreeClangPatch.cmake"
+    CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     INSTALL_COMMAND COMMAND ${CMAKE_SOURCE_DIR}/Build/Utils/Copy "${CMAKE_CURRENT_SOURCE_DIR}/BTree" "${CMAKE_BINARY_DIR}/External/include/btree" /s /d
 )
