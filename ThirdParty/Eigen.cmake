2ebbb7075d7fa413943396b2705797a0a51b2288
Miguel Petersen <miguel.pemail@gmail.com>
2023-12-19 12:24:23 +0100

    [Build] Added Eigen and STB libraries - Only pulled in experimental builds

---
+++ ThirdParty/Eigen.cmake
@@ -0,0 +1,38 @@
+# 
+# The MIT License (MIT)
+# 
+# Copyright (c) 2023 Miguel Petersen
+# Copyright (c) 2023 Advanced Micro Devices, Inc
+# Copyright (c) 2023 Fatalist Development AB
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# of this software and associated documentation files (the "Software"), to deal 
+# in the Software without restriction, including without limitation the rights 
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+# of the Software, and to permit persons to whom the Software is furnished to do so, 
+# subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all 
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# 
+
+# Linear algebra library
+ExternalProject_Add(
+    Eigen
+    GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
+    GIT_TAG 3.4.0
+    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/Eigen
+    USES_TERMINAL_INSTALL 0
+    UPDATE_DISCONNECTED ${ThirdPartyDisconnected}
+    CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/External
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+        -DCMAKE_MAKE_PROGRAM=${CMAKE_MAKE_PROGRAM}
+        -G ${CMAKE_GENERATOR}
+)
---
+++ ThirdParty/STB.cmake
@@ -0,0 +1,45 @@
+# 
+# The MIT License (MIT)
+# 
+# Copyright (c) 2023 Miguel Petersen
+# Copyright (c) 2023 Advanced Micro Devices, Inc
+# Copyright (c) 2023 Fatalist Development AB
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# of this software and associated documentation files (the "Software"), to deal 
+# in the Software without restriction, including without limitation the rights 
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+# of the Software, and to permit persons to whom the Software is furnished to do so, 
+# subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all 
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# 
+
+include(FetchContent)
+
+# Collection of single-file libraries
+FetchContent_Declare(
+    STB
+    GIT_REPOSITORY https://github.com/nothings/stb.git
+    GIT_TAG 03f50e343d796e492e6579a11143a085429d7f5d
+    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/STB
+)
+
+# Fetch the contents
+FetchContent_MakeAvailable(STB)
+
+# Local library for implementation
+add_library(STBLib STATIC STBLib/STBLib.cpp)
+
+# Includes
+target_include_directories(
+    STBLib SYSTEM PUBLIC
+    ${CMAKE_CURRENT_SOURCE_DIR}/STB
+)
