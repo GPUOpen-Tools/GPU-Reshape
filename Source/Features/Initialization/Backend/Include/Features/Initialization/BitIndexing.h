bde13dbb16220f559fc2c742172c62d099dd25be
Miguel Petersen <miguel.pemail@gmail.com>
2024-04-14 15:30:56 +0200

    [Initialization] Added non-atomic texel read

--- Source/Features/Initialization/Backend/Include/Features/Initialization/BitIndexing.h
+++ Source/Features/Initialization/Backend/Include/Features/Initialization/BitIndexing.h
@@ -65,3 +65,28 @@ static IL::ID AtomicAndTexelAddress(IL::Emitter<T>& emitter, IL::ID buffer, IL::
     // Only report the texel bit itself, ignore the rest
     return emitter.BitAnd(value, bit);
 }
+
+/// Perform a read of a texel address
+/// \param emitter instructione mitter
+/// \param buffer destination buffer
+/// \param baseElementAlign32 the base memory offset aligned to 32
+/// \param texelOffset intra-resource texel offset
+/// \return existing value
+template<typename T>
+static IL::ID ReadTexelAddress(IL::Emitter<T>& emitter, IL::ID buffer, IL::ID baseElementAlign32, IL::ID texelOffset) {
+    Backend::IL::ConstantMap& constants = emitter.GetProgram()->GetConstants();
+
+    // Extract element and global element
+    IL::ID element       = emitter.Div(texelOffset, constants.UInt(32)->id);
+    IL::ID globalElement = emitter.Add(baseElementAlign32, element);
+
+    // Perform atomic and at address
+    IL::ID value = emitter.Extract(emitter.LoadBuffer(emitter.Load(buffer), globalElement), constants.UInt(0)->id);
+
+    // Extract bit
+    IL::ID bitIndex = emitter.Rem(texelOffset, constants.UInt(32)->id);
+    IL::ID bit      = emitter.BitShiftLeft(constants.UInt(1u)->id, bitIndex);
+
+    // Only report the texel bit itself, ignore the rest
+    return emitter.BitAnd(value, bit);
+}
