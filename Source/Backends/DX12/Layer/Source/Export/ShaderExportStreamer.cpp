6791a748d5f45fd6db89d79bba36aaa7a4eefb26
Miguel Petersen <miguel.pemail@gmail.com>
2025-05-21 22:15:38 +0200

    [DX12] Fixed descriptor double-free

--- Source/Backends/DX12/Layer/Source/Export/ShaderExportStreamer.cpp
+++ Source/Backends/DX12/Layer/Source/Export/ShaderExportStreamer.cpp
@@ -1419,12 +1419,14 @@ void ShaderExportStreamer::ProcessDescriptorsNoLock() {
             }
 
             // Free the segments patch descriptors
-            sharedCPUHeapAllocator->Free(segment->patchDeviceCPUDescriptor);
-            sharedGPUHeapAllocator->Free(segment->patchDeviceGPUDescriptor);
-            
-            // Cleanup
-            segment->patchDeviceCPUDescriptor = {};
-            segment->patchDeviceGPUDescriptor = {};
+            if (segment->patchDeviceCPUDescriptor.cpuHandle.ptr) {
+                sharedCPUHeapAllocator->Free(segment->patchDeviceCPUDescriptor);
+                sharedGPUHeapAllocator->Free(segment->patchDeviceGPUDescriptor);
+                    
+                // Cleanup
+                segment->patchDeviceCPUDescriptor = {};
+                segment->patchDeviceGPUDescriptor = {};
+            }
         }
     }
 }
