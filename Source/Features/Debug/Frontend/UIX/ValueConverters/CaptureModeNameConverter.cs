291033ff2c41710c92545b71e22a5805b17bd196
Miguel Petersen <miguel.pemail@gmail.com>
2025-11-10 15:19:15 +0100

    [Debug] Added capture mode labels

---
+++ Source/Features/Debug/Frontend/UIX/ValueConverters/CaptureModeNameConverter.cs
@@ -0,0 +1,77 @@
+﻿// 
+// The MIT License (MIT)
+// 
+// Copyright (c) 2024 Advanced Micro Devices, Inc.,
+// Fatalist Development AB (Avalanche Studio Group),
+// and Miguel Petersen.
+// 
+// All Rights Reserved.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal 
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+// of the Software, and to permit persons to whom the Software is furnished to do so, 
+// subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all 
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using GRS.Features.Debug.UIX.Models;
+
+namespace Studio.ValueConverters
+{
+    public class CaptureModeNameConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert the value
+        /// </summary>
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is not BreakpointCaptureMode captureMode)
+            {
+                return null;
+            }
+
+            // Tooltip?
+            if (parameter == "tooltip")
+            {
+                return captureMode switch
+                {
+                    BreakpointCaptureMode.FirstEvent => "Capture on the first draw/dispatch it hits",
+                    BreakpointCaptureMode.FirstViewport => "Capture on all events inside the first viewport it hits",
+                    BreakpointCaptureMode.AllEvents => "Capture all events as loose data items (flat list of values)",
+                    _ => captureMode.ToString()
+                };
+            }
+            else
+            {
+                return captureMode switch
+                {
+                    BreakpointCaptureMode.FirstEvent => "First Event",
+                    BreakpointCaptureMode.FirstViewport => "First Viewport",
+                    BreakpointCaptureMode.AllEvents => "All Events (Loose)",
+                    _ => captureMode.ToString()
+                };
+            }
+        }
+
+        /// <summary>
+        /// Convert the value back
+        /// </summary>
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
