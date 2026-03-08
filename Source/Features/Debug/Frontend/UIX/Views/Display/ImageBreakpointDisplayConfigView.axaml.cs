a08d322805fe23addd6466620196c0bac0282bd2
Miguel Petersen <miguel.pemail@gmail.com>
2026-03-08 19:49:19 +0100

    [Debug] Added missing changes

--- Source/Features/Debug/Frontend/UIX/Views/Display/ImageBreakpointDisplayConfigView.axaml.cs
+++ Source/Features/Debug/Frontend/UIX/Views/Display/ImageBreakpointDisplayConfigView.axaml.cs
@@ -1,5 +1,8 @@
+using System;
 using Avalonia.Controls;
+using GRS.Features.Debug.UIX.ViewModels;
 using ReactiveUI;
+using Studio.Extensions;
 
 namespace UIX.Views.Display;
 
@@ -14,5 +17,26 @@ public partial class ImageBreakpointDisplayConfigView : UserControl, IViewFor
     public ImageBreakpointDisplayConfigView()
     {
         InitializeComponent();
+
+        // Bind to events
+        this.WhenAnyValue(x => x.DataContext)
+            .CastNullable<ImageBreakpointDisplayViewModel>()
+            .Subscribe(vm =>
+            {
+                AutoRange.Events().Click.Subscribe(_ =>
+                {
+                    if (vm.Inspector == null)
+                    {
+                        return;
+                    }
+        
+                    // Summarize the range across the whole image
+                    PixelValueRange range = vm.Inspector.SummarizeRange();
+                    DisplayRangeSlider.Minimum = range.MinValue;
+                    DisplayRangeSlider.LowerValue = range.MinValue;
+                    DisplayRangeSlider.Maximum = range.MaxValue;
+                    DisplayRangeSlider.UpperValue = range.MaxValue;
+                });
+            });
     }
 }
